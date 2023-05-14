f4cbde421f03a0c45af4d059b5fee21a30a34b6d
Francisco <ferreiradeveloper@gmail.com>
2023-05-14 16:50:42 +0200

    add listas.py

---
+++ listas.py
@@ -0,0 +1,11 @@
+# las listas son colecciones ordenadas de datos y por tanto el orden en que se encuentran sus elementos es importante,
+# la promera posición ocupada es la 0 (tipico en los vectores). La listas en python son mutables, tu type es list y se crean
+# con corchetes y y sus elementos son separados por coma ([item1, item2....]).
+
+nombre_lista = [1,'Francisco',True,21.10, ['a','b','c'], {'edad': 54, 'apellido': 'ferreira', 'casado': True}, (1,1,2,2,5,5)]
+
+print(nombre_lista)
+print(nombre_lista[4])
+print(nombre_lista[-1])
+
+
