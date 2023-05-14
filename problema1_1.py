87434bf764629972739c9cd537bd97cbff79d00f
Francisco <ferreiradeveloper@gmail.com>
2023-05-15 01:23:53 +0200

    add problea 1.1

---
+++ problema1_1.py
@@ -0,0 +1,5 @@
+'''
+Escriba una función que reciba una lista y que regrese como resultado una tupla almacenado
+el primer y el último elemento de la lista. En caso de que la lista no tenga dos o mas 
+elementos, debe lanzar una excepción.
+'''
