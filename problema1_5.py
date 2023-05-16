2fb43127c48524d5c97447b5db329191a64eeed5
Francisco <ferreiradeveloper@gmail.com>
2023-05-17 00:42:15 +0200

    correcciom nombre problema 1.4

---
+++ problema1_5.py
@@ -0,0 +1,6 @@
+'''
+Escriba una función que reciba una lista y que regrese True si en la
+lista hay, por lo menos, dos elementos tipo cadena de caracteres (str).
+En caso contrario deberá regresar False. Además, debe escribir un programa 
+para probar la función desarrollada.
+'''
